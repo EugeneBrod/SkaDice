b19348baf7c2e505e1ebf883ec13b2722f6ddc2e
EugeneBrod <eugenebrod@gmail.com>
2021-08-17 14:27:50 -0700

    Initial Commit

---
+++ SkateDie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkateDie
+//
+//  Created by Eugene Brodsky on 8/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
