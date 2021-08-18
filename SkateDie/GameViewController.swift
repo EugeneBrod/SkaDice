6942dc3d2f97053111282621fe650a1059843294
EugeneBrod <eugenebrod@gmail.com>
2021-08-18 14:11:10 -0700

    Stable functionality, now working on UI design.

---
+++ SkateDie/GameViewController.swift
@@ -0,0 +1,78 @@
+//
+//  Game.swift
+//  SkateDie
+//
+//  Created by Eugene Brodsky on 8/17/21.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    
+    var repeats = false
+    var tasteful = false
+    var tricks = [String]()
+    
+    @IBOutlet weak var trickLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.readInTricks()
+    }
+    
+    @IBAction func roll(_ sender: Any) {
+        if (tricks.count == 0) {
+            trickLabel.text = "Out of Tricks!"
+            return
+        }
+        let randomidx = Int.random(in: 0..<tricks.count)
+        trickLabel.text = tricks[randomidx]
+        if (!self.repeats) {
+            tricks.remove(at: randomidx)
+        }
+    }
+    
+
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated:true, completion:nil)
+    }
+    
+    
+    func readInTricks() {
+        if let path = Bundle.main.path(forResource: "tricks", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                let myStrings = data.components(separatedBy: .newlines)
+                if (!self.tasteful) {
+                    for (idx, trick) in myStrings.enumerated() {
+                        if (idx%2==0) {
+                            self.tricks.append(trick)
+                        }
+                    }
+                }
+                else {
+                    for index in 0...myStrings.count-2 {
+                        if (index%2 == 1) {
+                            continue
+                        }
+                        else {
+                            if (myStrings[index + 1] == "0") {
+                                continue
+                            }
+                            else {
+                                self.tricks.append(myStrings[index])
+                            }
+                        }
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+
+    
+    
+
+}
--- SkateDie/ViewController.swift
+++ SkateDie/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    
+    var tasteful = true
+    var repeats = false
+    var count = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func repeatsswitch(_ sender: UISwitch) {
+        if (sender.isOn) {
+            repeats = true
+        }
+        else {
+            repeats = false
+        }
+    }
+    
+    @IBAction func tastefulswitch(_ sender: UISwitch) {
+        if (sender.isOn) {
+            tasteful = true
+        }
+        else {
+            tasteful = false
+        }
+    }
+    @IBAction func startGame(_ sender: UIButton) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
+        vc.repeats = self.repeats
+        vc.tasteful = self.tasteful
+        show(vc, sender:self)    }
 }
 
