b784a80cc6e943774eb7424eff05c1032e349c25
EugeneBrod <eugenebrod@gmail.com>
2021-08-18 22:36:35 -0700

    ReDesigned, TODO: animate transition to game, add tricks, animate dice

--- SkateDie/ViewController.swift
+++ SkateDie/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var settingsLabel: UILabel!
     
     var tasteful = true
     var repeats = false
@@ -18,26 +19,98 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let img = UIImage(named: "orange_wheel")
+        wheelGraphic.image = img
+        wheelGraphic.isUserInteractionEnabled = true
+        
+        let img2 = UIImage(named: "pink_wheel")
+        wheelGraphic2.image = img2
+        wheelGraphic2.isUserInteractionEnabled = true
+        
+        settingsLabel.text = "NO REPEATS AND TASTEFUL"
     }
     
     
-    @IBAction func repeatsswitch(_ sender: UISwitch) {
-        if (sender.isOn) {
-            repeats = true
+    @IBOutlet weak var wheelGraphic: UIImageView!
+    
+    @IBOutlet weak var wheelGraphic2: UIImageView!
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        let touch:UITouch = touches.first!
+
+        if touch.view === wheelGraphic {
+
+
+            let position = touch.location(in: self.view)
+            let target = wheelGraphic.center
+            let angle = atan2(target.y-position.y, target.x-position.x)
+            wheelGraphic.transform = CGAffineTransform(rotationAngle: angle)
+            upDateRepeatSetting(angle: Double(angle))
+
+
+        }
+        
+        if touch.view === wheelGraphic2 {
+
+
+            let position = touch.location(in: self.view)
+            let target = wheelGraphic2.center
+            let angle = atan2(target.y-position.y, target.x-position.x)
+            wheelGraphic2.transform = CGAffineTransform(rotationAngle: angle)
+            upDateTastefulSetting(angle: Double(angle))
+
+
+        }
+    }
+
+    func upDateRepeatSetting(angle: Double ) {
+        if (angle >= -(Double.pi/2) && angle <= (Double.pi/2)) {
+            repeats = false
+            if (tasteful) {
+                settingsLabel.text = "NO REPEATS AND TASTEFUL"
+            }
+            else {
+                settingsLabel.text = "NO REPEATS AND TASTELESS"
+            }
         }
         else {
-            repeats = false
+            repeats = true
+            if (tasteful) {
+                settingsLabel.text = "REPEATS AND TASTEFUL"
+            }
+            else {
+                settingsLabel.text = "REPEATS AND TASTELESS"
+            }
         }
+     
     }
     
-    @IBAction func tastefulswitch(_ sender: UISwitch) {
-        if (sender.isOn) {
+    func upDateTastefulSetting(angle: Double ) {
+        if (angle >= -(Double.pi/2) && angle <= (Double.pi/2)) {
             tasteful = true
+            if (repeats) {
+                settingsLabel.text = "REPEATS AND TASTEFUL"
+            }
+            else {
+                settingsLabel.text = "NO REPEATS AND TASTEFUL"
+            }
         }
         else {
             tasteful = false
+            if (repeats) {
+                settingsLabel.text = "REPEATS AND TASTELESS"
+            }
+            else {
+                settingsLabel.text = "NO REPEATS AND TASTELESS"
+            }
         }
+     
     }
+
+        
+
     @IBAction func startGame(_ sender: UIButton) {
         let vc = self.storyboard?.instantiateViewController(withIdentifier: "GameViewController") as! GameViewController
         vc.repeats = self.repeats
